f7d7d2d86a3ddb0b5f5423cbefbb9604abdc33ab
traba49 <ptrabak@gmail.com>
2023-03-20 17:54:41 +0200

    Update ModuleHolder.cs

--- Assets/Scripts/TowerCreator/ModuleHolder.cs
+++ Assets/Scripts/TowerCreator/ModuleHolder.cs
@@ -1,29 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace TowerCreatorNS
 {
     public class ModuleHolder : MonoBehaviour
     {
         public ModuleTypes Types;
+        public static readonly Dictionary<ModuleTypes, Type> EffectTypeMap = new Dictionary<ModuleTypes, Type>
+        {
+            { ModuleTypes.damage, typeof(EffectModuleDamage) },
+            { ModuleTypes.HP_up, typeof(EffectModuleHpUp) },
+            { ModuleTypes.slowdown, typeof(EffectModuleSlowdown) }
+        };
 
         public object ReturnType()
         {
-            switch (Types)
+            if (EffectTypeMap.TryGetValue(Types, out Type effectType))
             {
-                case ModuleTypes.damage:
-                    EffectModuleDamage e = new EffectModuleDamage();
-                    return e;
-                case ModuleTypes.HP_up:
-                    EffectModuleHpUp r = new EffectModuleHpUp();
-                    return r;
-                case ModuleTypes.slowdown:
-                    EffectModuleSlowdown t = new EffectModuleSlowdown();
-                    return t;
-                default:
-                    return null;
+                return effectType;
             }
+            return null;
         }
 
         private void OnDisable()
